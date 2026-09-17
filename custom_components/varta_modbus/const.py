81f25275dc5ff172f209cb31235c0cc65af1f17b
DAR <andreasreinhardt81@gmail.com>
2026-09-17 20:19:45 +0200

    Update const.py

--- custom_components/varta_modbus/const.py
+++ custom_components/varta_modbus/const.py
@@ -9,8 +9,4 @@ CONF_UNIT_ID = "unit_id"
 DEFAULT_PORT = 502
 DEFAULT_UNIT_ID = 1
 
-# VARTA external power-control watchdog.
-#
-# Writing register 1074 or 1075 starts a 120 second timeout.
-# Refresh the active limits every 60 seconds.
-VARTA_EXTERNAL_CONTROL_TIMEOUT = 120
+
